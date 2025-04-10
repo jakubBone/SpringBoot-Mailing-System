4f9e7320a6d1f600654e8415d2dbc99e4cb0a1f3
jakubBone <jakub.bone1990@gmail.com>
2025-04-10 11:11:03 +0200

    chore: added User entity

---
+++ src/main/java/com/jakubbone/domain/model/User.java
@@ -0,0 +1,32 @@
+package com.jakubbone.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor // JPA/Hibernate require to create object getting from DB
+public class User {
+
+    enum Role { USER, ADMIN }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING) // Hibernate save as a String, not a number
+    private Role role;
+}
