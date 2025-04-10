5fd2ee075eebb3494f7961af5c723ae433f6087f
jakubBone <jakub.bone1990@gmail.com>
2025-04-10 11:22:57 +0200

    feat: implemented UserRepository with findByUsername method

---
+++ src/main/java/com/jakubbone/reposotory/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jakubbone.reposotory;
+
+import com.jakubbone.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// JPARepository provides the ready methods
+// e.g. save(), findById(), findAll(), delete()
+// for User Entity with the key type Long
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username)
+}
