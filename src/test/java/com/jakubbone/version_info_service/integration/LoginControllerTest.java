444042a1e7d4353e6302eca5b7ee171080d7aee2
jakubBone <jakub.bone1990@gmail.com>
2025-04-20 06:51:54 +0200

    test: added unit InfoControllerTest

--- src/test/java/com/jakubbone/version_info_service/integration/LoginControllerTest.java
+++ src/test/java/com/jakubbone/version_info_service/integration/LoginControllerTest.java
@@ -3,7 +3,6 @@ package com.jakubbone.version_info_service.integration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.jakubbone.dto.LoginRequest;
-import com.jakubbone.utils.EnvConfigLoader;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,7 +14,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+
+@SpringBootTest // @SpringBootTest runs all components with all Spring configuration
 @AutoConfigureMockMvc
 class LoginControllerTest {
     ObjectMapper mapper = new ObjectMapper();
---
+++ src/test/java/com/jakubbone/version_info_service/unit/LoginControllerTest.java
@@ -0,0 +1,5 @@
+package com.jakubbone.version_info_service.unit;
+
+public class LoginControllerTest {
+
+}
