4626e3b80750a1be7bb7f48bb2b23a3a1f83f796
jakubBone <jakub.bone1990@gmail.com>
2025-04-22 16:42:07 +0200

    refactor: renamed main class

--- src/main/java/com/jakubbone/MailingApp.java
+++ src/main/java/com/jakubbone/MailingApp.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class CurrencyExchangeApp {
+public class MailingApp {
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyExchangeApp.class, args);
+		SpringApplication.run(MailingApp.class, args);
 	}
 }
