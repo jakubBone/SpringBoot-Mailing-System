925557b8a9a8b2714b744d5c650b70e17bc6f99e
jakubBone <jakub.bone1990@gmail.com>
2025-04-23 11:45:26 +0200

    test: added LoginConteroller unit test

--- src/main/java/com/jakubbone/dto/LoginRequest.java
+++ src/main/java/com/jakubbone/dto/LoginRequest.java
@@ -1,11 +1,13 @@
 package com.jakubbone.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor // @NoArgsConstructor - Jackson requires no args constructor to create JSON request
 public class LoginRequest {
     private String username;
---
+++ src/test/java/com/jakubbone/version_info_service/unit/LoginControllerTest.java
@@ -0,0 +1,78 @@
+package com.jakubbone.version_info_service.unit;
+
+import com.jakubbone.controller.LoginController;
+import com.jakubbone.dto.LoginRequest;
+import com.jakubbone.model.User;
+import com.jakubbone.reposotory.UserRepository;
+import com.jakubbone.utils.JwtTokenProvider;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class LoginControllerTest {
+
+    @InjectMocks
+    LoginController loginController;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Test
+    void shouldReturnToken_whenLoginSuccess_ReturnsToken() {
+        LoginRequest req = new LoginRequest("alice", "password123");
+        User user = new User("alice", "testHashedPassword", "USER");
+
+        when(userRepository.findByUsername("alice")).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("password123", user.getPasswordHash())).thenReturn(true);
+        when(jwtTokenProvider.createToken("alice", "USER")).thenReturn("fake-jwt-token");
+
+        ResponseEntity <?> resp = loginController.login(req);
+        Map respBody = (Map) resp.getBody();
+
+        assertEquals(HttpStatus.OK, resp.getStatusCode());
+        assertEquals("fake-jwt-token", respBody.get("token"));
+    }
+
+    @Test
+    void shouldReturn401_whenPasswordIncorrect() {
+        LoginRequest req = new LoginRequest("alice", "incorrectPassword");
+        User user = new User("alice", "testHashedPassword", "USER");
+
+        when(userRepository.findByUsername("alice")).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("incorrectPassword", user.getPasswordHash())).thenReturn(false);
+
+        ResponseEntity <?> resp = loginController.login(req);
+
+        assertEquals(HttpStatus.UNAUTHORIZED, resp.getStatusCode());
+    }
+
+    @Test
+    void shouldReturn401_whenUserIncorrect() {
+        LoginRequest req = new LoginRequest("incorrectUser", "password123");
+        User user = new User("incorrectUser", "password123", "USER");
+
+        when(userRepository.findByUsername("incorrectUser")).thenReturn(Optional.empty());
+
+        ResponseEntity <?> resp = loginController.login(req);
+
+        assertEquals(HttpStatus.UNAUTHORIZED, resp.getStatusCode());
+    }
+}
