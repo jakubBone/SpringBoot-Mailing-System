2b5eb454c23e217b6c34d2d605b57008b284c8a5
jakubBone <jakub.bone1990@gmail.com>
2025-04-28 10:38:16 +0200

    refactor: corrected naming

--- src/main/java/com/jakubbone/SpringBootMailingApplication.java
+++ src/main/java/com/jakubbone/SpringBootMailingApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class MailingApp {
+public class SpringBootMailingApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MailingApp.class, args);
+		SpringApplication.run(SpringBootMailingApplication.class, args);
 	}
 }
