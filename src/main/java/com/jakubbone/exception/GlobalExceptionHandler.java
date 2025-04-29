2ec19363fed79fcdba0b89b3976832c432155e5d
jakubBone <jakub.bone1990@gmail.com>
2025-04-29 08:40:44 +0200

    feat: implemented initial @ExceptionHandler methods

--- src/main/java/com/jakubbone/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jakubbone/exception/GlobalExceptionHandler.java
@@ -1,4 +1,43 @@
 package com.jakubbone.exception;
 
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
+@Log4j2
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<String> handleJwtException(JwtException e){
+        //log.error("Database error occurred: {}", e.getMessage(), e);
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid JWT token");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e){
+        //log.error("Database error occurred: {}", e.getMessage(), e);
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid argument: " + e.getMessage());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException e){
+        //log.error("Database error occurred: {}", e.getMessage(), e);
+
+        return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> handleDataAccessException(DataAccessException e) {
+        //log.error("Database error occurred: {}", e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+    }
 }
