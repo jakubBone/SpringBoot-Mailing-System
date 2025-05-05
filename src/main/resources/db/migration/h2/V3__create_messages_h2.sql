9c0618e685ce811f017d4d647dfe6302ea48f04f
jakubBone <jakub.bone1990@gmail.com>
2025-05-05 14:41:27 +0200

    refactor: corrected sql queries in Flyway scripts

--- src/main/resources/db/migration/h2/V3__create_messages_h2.sql
+++ src/main/resources/db/migration/h2/V3__create_messages_h2.sql
@@ -1,17 +1,17 @@
 CREATE TABLE messages (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     sender_id BIGINT NOT NULL,
     recipient_id BIGINT NOT NULL,
     content TEXT NOT NULL,
-    timestamp DATETIME NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
 
     CONSTRAINT fk_sender
-        FOREIGN KEY (sender_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE,
+      FOREIGN KEY (sender_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE,
 
     CONSTRAINT fk_recipient
-        FOREIGN KEY (recipient_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
+      FOREIGN KEY (recipient_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE
 );
