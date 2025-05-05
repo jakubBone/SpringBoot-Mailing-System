58d7ff298fae12afcb2c1692b832c3493de39bf7
jakubBone <jakub.bone1990@gmail.com>
2025-05-05 13:02:16 +0200

    feat: implemented basic fields to SendMessageRequest

--- src/main/java/com/jakubbone/dto/SendMessageRequest.java
+++ src/main/java/com/jakubbone/dto/SendMessageRequest.java
@@ -1,4 +1,16 @@
 package com.jakubbone.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SendMessageRequest {
+    @NotBlank(message = "Recipient username cannot be blank")
+    private String to;
+
+    @NotBlank(message = "Message text cannot be blank")
+    private String text;
+
 }
