aeb3a752b709da757f1c979390b6be5368226f77
jakubBone <jakub.bone1990@gmail.com>
2025-05-06 11:44:29 +0200

    feat: updated Flyway h2 DB script by inserting new initial user 'jakub'

--- src/main/resources/db/migration/h2/V2__insert_default_admin_h2.sql
+++ src/main/resources/db/migration/h2/V2__insert_default_admin_h2.sql
@@ -1,2 +1,3 @@
 INSERT INTO users (username, password_hash, role)
-VALUES ('admin', '$2a$10$4fgXJJitASt8SeKGpw.8JeboCnx4EByb7M5A5e.yVbc7W3OLipUzy', 'ADMIN');
+VALUES ('admin', '$2a$10$4fgXJJitASt8SeKGpw.8JeboCnx4EByb7M5A5e.yVbc7W3OLipUzy', 'ADMIN'),
+        ('jakub', '$2a$10$4fgXJJitASt8SeKGpw.8JeboCnx4EByb7M5A5e.yVbc7W3OLipUzy', 'USER');
