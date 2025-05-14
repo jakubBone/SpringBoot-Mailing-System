e9390234b08ad2e733aa40af81c6c3ca5f683479
jakubBone <jakub.bone1990@gmail.com>
2025-05-14 14:32:40 +0200

    refactor: corrected endpoints structure in filter chain

--- src/main/java/com/jakubbone/config/SecurityConfig.java
+++ src/main/java/com/jakubbone/config/SecurityConfig.java
@@ -38,8 +38,8 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login/v1/impersonate").hasRole("ADMIN")
-                        .requestMatchers("/logout/v1/impersonate").authenticated()
+                        .requestMatchers("api/login/v1/impersonate").hasRole("ADMIN")
+                        .requestMatchers("api/logout/v1/impersonate").authenticated()
                         .requestMatchers(("/api/v1/messages")).hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/api/v1/login", "/api/v1/register", "/api/v1/info", "/api/v1/uptime").permitAll()
                         .anyRequest().authenticated()
