ecc7621746a8dd04aef5ca55b88a0551bd276900
jakubBone <jakub.bone1990@gmail.com>
2025-05-16 12:35:48 +0200

    feat: added ImpersonationController

---
+++ src/main/java/com/jakubbone/controller/ImpersonationController.java
@@ -0,0 +1,11 @@
+package com.jakubbone.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/impersonate")
+public class ImpersonationController {
+
+
+}
