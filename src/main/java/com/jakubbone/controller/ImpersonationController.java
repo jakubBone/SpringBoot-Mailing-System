eb9644ae3196b7ae7dc38f68f0f22b9308b8cc3d
jakubBone <jakub.bone1990@gmail.com>
2025-05-16 12:42:24 +0200

    feat: implemented impersonate method frame
    Former-commit-id: 78b531aa3aaa60818ede2aa289bef96b6c88bd28

--- src/main/java/com/jakubbone/controller/ImpersonationController.java
+++ src/main/java/com/jakubbone/controller/ImpersonationController.java
@@ -1,11 +1,33 @@
 package com.jakubbone.controller;
 
+import com.jakubbone.dto.LoginRequest;
+import com.jakubbone.dto.SendMessageRequest;
+import com.jakubbone.repository.UserRepository;
+import com.jakubbone.utils.JwtTokenProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/impersonate")
+@RequestMapping("api/v1")
 public class ImpersonationController {
+    private final PasswordEncoder encoder;
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
+    public ImpersonationController(PasswordEncoder encoder, UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
+        this.encoder = encoder;
+        this.userRepository = userRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login/impersonate")
+    public ResponseEntity<?> impersonate(@RequestBody LoginRequest req, Authentication authentication){
+
+    }
 
 }
