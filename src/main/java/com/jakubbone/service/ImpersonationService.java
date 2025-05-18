2865413ef108f4813887443612b5e79e4f4571dc
jakubBone <jakub.bone1990@gmail.com>
2025-05-18 09:08:06 +0200

    refactor: removed redundant code

--- src/main/java/com/jakubbone/service/ImpersonationService.java
+++ src/main/java/com/jakubbone/service/ImpersonationService.java
@@ -4,12 +4,8 @@ import com.jakubbone.model.User;
 import com.jakubbone.repository.UserRepository;
 import com.jakubbone.utils.JwtTokenProvider;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Collections;
-import java.util.Map;
 
 @Service
 public class ImpersonationService {
--- src/main/java/com/jakubbone/utils/JwtTokenProvider.java
+++ src/main/java/com/jakubbone/utils/JwtTokenProvider.java
@@ -1,6 +1,5 @@
 package com.jakubbone.utils;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Getter;
