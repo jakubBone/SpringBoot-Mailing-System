e6cd6466b31b493d191581b182aed3fb7f567981
jakubBone <jakub.bone1990@gmail.com>
2025-05-23 16:38:37 +0200

    refactor: removed MessageService interface
    Former-commit-id: 905f602eaa4ef6ddba5c2ba78a41e6b442f5347a

--- src/main/java/com/jakubbone/service/MessageService.java
+++ src/main/java/com/jakubbone/service/MessageService.java
@@ -1,7 +1,34 @@
 package com.jakubbone.service;
 
 import com.jakubbone.model.Message;
+import com.jakubbone.repository.MessageRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-public interface MessageService {
-    Message sendMessage(String senderUsername, String recipientUsername, String content);
+import java.time.LocalDateTime;
+
+@Service
+public class MessageService {
+    private final MessageRepository messageRepository;
+    private final KeycloakService keycloakUserService;
+
+    public MessageService(MessageRepository messageRepository, KeycloakService keycloakUserService) {
+        this.messageRepository = messageRepository;
+        this.keycloakUserService = keycloakUserService;
+    }
+
+    public Message sendMessage(String fromUsername, String toUsername, String content) {
+        if (!keycloakUserService.existsByUsername(toUsername)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipient " + toUsername + " not found");
+        }
+
+        Message msg = new Message();
+        msg.setSenderUsername(fromUsername);
+        msg.setRecipientUsername(toUsername);
+        msg.setContent(content);
+        msg.setTimestamp(LocalDateTime.now());
+
+        return messageRepository.save(msg);
+    }
 }
