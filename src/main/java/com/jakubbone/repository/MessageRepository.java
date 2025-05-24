7eb90e5c55b4585a4e5d8f8763f5694b3add3a78
jakubBone <jakub.bone1990@gmail.com>
2025-05-24 17:22:28 +0200

    refactor: add comments

--- src/main/java/com/jakubbone/repository/MessageRepository.java
+++ src/main/java/com/jakubbone/repository/MessageRepository.java
@@ -3,6 +3,9 @@ package com.jakubbone.repository;
 import com.jakubbone.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// JPARepository provides the ready methods
+// e.g. save(), findById(), findAll(), delete()
+// for Message Entity with the key type Long
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
 }
