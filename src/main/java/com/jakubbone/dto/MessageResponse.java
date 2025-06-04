3c981f648afefe9c1ddb6c69e7127f4277273468
jakubBone <jakub.bone1990@gmail.com>
2025-06-04 14:52:15 +0200

    feat: implemeneted MessageResponse DTO
    Former-commit-id: 39642002fdc1eda3825774b8b3ecb6cbded7ede3

--- src/main/java/com/jakubbone/dto/MessageResponse.java
+++ src/main/java/com/jakubbone/dto/MessageResponse.java
@@ -1,4 +1,29 @@
 package com.jakubbone.dto;
 
+import com.jakubbone.model.Message;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class MessageResponse {
+    private Long id;
+    private String senderUsername;
+    private String recipientUsername;
+    private String content;
+    private LocalDateTime timestamp;
+    private boolean isRead;
+
+    public static MessageResponse fromEntity(Message msg) {
+        return new MessageResponse(
+                msg.getId(),
+                msg.getSenderUsername(),
+                msg.getRecipientUsername(),
+                msg.getContent(),
+                msg.getTimestamp(),
+                msg.isRead()
+        );
+    }
 }
--- src/main/java/com/jakubbone/model/Message.java
+++ src/main/java/com/jakubbone/model/Message.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(name = "sender_id", nullable = false)
     private String senderUsername;
@@ -32,5 +32,4 @@ public class Message {
     private LocalDateTime timestamp;
 
     private boolean isRead = false;
-
 }
