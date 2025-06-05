8e210cfb9f14be5f2f97b4045f4a77ec9c28d732
jakubBone <jakub.bone1990@gmail.com>
2025-06-05 10:25:16 +0200

    feat: impleneted message size validation and OWASP library with sanitaizing message
    Former-commit-id: 4a972e92b2e706c910efbfea4ddd4e3f32ef8b94

--- src/main/java/com/jakubbone/service/MessageService.java
+++ src/main/java/com/jakubbone/service/MessageService.java
@@ -1,5 +1,8 @@
 package com.jakubbone.service;
 
+import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
+
 import com.jakubbone.model.Message;
 import com.jakubbone.repository.MessageRepository;
 import org.springframework.http.HttpStatus;
@@ -13,6 +16,8 @@ public class MessageService {
     private final MessageRepository messageRepository;
     private final KeycloakService keycloakUserService;
 
+    private final PolicyFactory sanitizer = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
+
     public MessageService(MessageRepository messageRepository, KeycloakService keycloakUserService) {
         this.messageRepository = messageRepository;
         this.keycloakUserService = keycloakUserService;
@@ -23,10 +28,12 @@ public class MessageService {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipient " + toUsername + " not found");
         }
 
+        String sanitizedContent = sanitizer.sanitize(content);
+
         Message msg = new Message();
         msg.setSenderUsername(fromUsername);
         msg.setRecipientUsername(toUsername);
-        msg.setContent(content);
+        msg.setContent(sanitizedContent);
         msg.setTimestamp(LocalDateTime.now());
 
         return messageRepository.save(msg);
