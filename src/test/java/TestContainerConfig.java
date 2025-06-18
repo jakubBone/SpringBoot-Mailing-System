d738f2cf7a278c9adddfec5e8c1c463b63bc21c6
jakubBone <jakub.bone1990@gmail.com>
2025-06-18 14:42:19 +0200

    config: implemented basic configuration for TestContainerConfig class

---
+++ src/test/java/TestContainerConfig.java
@@ -0,0 +1,19 @@
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+public class TestContainerConfig {
+
+    @Container
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("spring_db")
+            .withUsername("spring_user")
+            .withPassword("spring123");
+
+    @Container
+    public static KeycloakContainer<?> keycloak = new KeycloakContainer("quay.io/keycloak/keycloak:26.2.4")
+            .withRealmImportFile("mailingsystem-realm.json")
+            .withEnv("KEYCLOAK_ADMIN", "admin")
+            .withEnv("KEYCLOAK_ADMIN_PASSWORD", "admin");
+
+}
