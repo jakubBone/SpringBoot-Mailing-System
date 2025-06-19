624decddf117773a11a9512084e1fc7e19164977
jakubBone <jakub.bone1990@gmail.com>
2025-06-19 14:04:20 +0200

    feat: Added DynamicPropertySource for TestContainers

--- src/test/java/TestContainerConfig.java
+++ src/test/java/TestContainerConfig.java
@@ -1,4 +1,6 @@
 import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 
@@ -16,4 +18,19 @@ public class TestContainerConfig {
             .withEnv("KEYCLOAK_ADMIN", "admin")
             .withEnv("KEYCLOAK_ADMIN_PASSWORD", "admin");
 
+    @DynamicPropertySource
+    static void registerDynamicProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+        registry.add("keycloak.base-url", () -> keycloak.getAuthServerUrl());
+        registry.add("keycloak.realm", () -> "mailingsystem"); /
+        registry.add("keycloak.admin-client-id", () -> "admin-cli");
+        registry.add("keycloak.admin-client-secret", () -> "YOUR_ADMIN_CLIENT_SECRET");
+        registry.add("keycloak.resource", () -> "account");
+        registry.add("keycloak.auth-server-url", () -> keycloak.getAuthServerUrl());
+        registry.add("keycloak.ssl-required", () -> "none");
+        registry.add("keycloak.public-client", () -> "true");
+    }
 }
