ec21d4326ddef0e428202acdab8e9d2728126b7b
jakubBone <jakub.bone1990@gmail.com>
2025-06-27 14:53:47 +0200

    chore: deleted redundant Flyway h2 migration script

--- src/main/resources/db/migration/h2/V1__create_messages_h2.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE messages (
-    id BIGSERIAL PRIMARY KEY,
-    sender_id VARCHAR NOT NULL,
-    recipient_id VARCHAR NOT NULL,
-    content TEXT NOT NULL,
-    is_read BOOLEAN DEFAULT FALSE,
-    timestamp TIMESTAMP NOT NULL
-);
