9ecfdbe728bd80a2c71aca36eb7bb57afe4899c4
jakubBone <jakub.bone1990@gmail.com>
2025-06-28 14:17:17 +0200

    feat: implemented message counting method in MessageRepository

--- src/main/java/com/jakubbone/repository/MessageRepository.java
+++ src/main/java/com/jakubbone/repository/MessageRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // e.g. save(), findById(), findAll(), delete()
 // for Message Entity with the key type Long
 public interface MessageRepository extends JpaRepository<Message, Long> {
-
+    // Spring Data JPA has a built-in parser that analyzes method names according to a set pattern
+    long countByRecipientIdAndIsReadFalse(String recipientId);
 }
