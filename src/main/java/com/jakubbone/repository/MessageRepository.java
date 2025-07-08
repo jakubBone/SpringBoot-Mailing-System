dd9e6fad9243952092b5eb17338709b964ac9112
jakubBone <jakub.bone1990@gmail.com>
2025-07-08 10:57:01 +0200

    feat: added readMessages feature with endpoint, service and repository implementation

--- src/main/java/com/jakubbone/repository/MessageRepository.java
+++ src/main/java/com/jakubbone/repository/MessageRepository.java
@@ -1,9 +1,13 @@
 package com.jakubbone.repository;
 
 import com.jakubbone.model.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     // Counts unread messages for a given recipient
     long countByRecipientIdAndIsReadFalse(String recipientId);
+
+    Page<Message> findByRecipientId(String recipientId, Pageable pageable);
 }
