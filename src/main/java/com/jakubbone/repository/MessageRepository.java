5487a2fdc2fd3b538de0ae3544d69dbc7bac2b0b
jakubBone <jakub.bone1990@gmail.com>
2025-07-14 14:08:18 +0200

    feat: added PESSIMISTIC_READ to MessageRepository message counting method
    Former-commit-id: be707433df75bc099ab11f3439563f81cf36cc43

--- src/main/java/com/jakubbone/repository/MessageRepository.java
+++ src/main/java/com/jakubbone/repository/MessageRepository.java
@@ -1,12 +1,15 @@
 package com.jakubbone.repository;
 
 import com.jakubbone.model.Message;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     // Counts unread messages for a given recipient
+    @Lock(LockModeType.PESSIMISTIC_READ)
     long countByRecipientIdAndIsReadFalse(String recipientId);
 
     Page<Message> findByRecipientId(String recipientId, Pageable pageable);
