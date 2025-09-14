e6bcd32a6566c5345e79f4d416049b9a26a72dab
jakubBone <jakub.bone1990@gmail.com>
2025-09-14 10:33:18 +0200

    test: add shouldThrowException_whenSSendingToSelf unit test

--- src/test/java/com/jakubbone/unit/MessageServiceTest.java
+++ src/test/java/com/jakubbone/unit/MessageServiceTest.java
@@ -3,10 +3,16 @@ package com.jakubbone.unit;
 import com.jakubbone.repository.MessageRepository;
 import com.jakubbone.service.KeycloakUserService;
 import com.jakubbone.service.MessageService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class MessageServiceTest {
@@ -18,4 +24,19 @@ class MessageServiceTest {
 
     @InjectMocks
     MessageService messageService;
+
+    @Test
+    void shouldThrowException_whenSSendingToSelf() {
+        String sender = "testuser";
+        String recipient = "testuser";
+        String content = "Hello";
+
+        ResponseStatusException ex = assertThrows(
+                ResponseStatusException.class,
+                () -> messageService.send(sender, recipient, content)
+        );
+
+        assertEquals("Cannot send message to yourself", ex.getReason());
+        assertEquals(HttpStatus.BAD_REQUEST, ex.getStatusCode());
+    }
 }
