d1f5a0cc6f9ace38dcce83caa6677a435623e49e
jakubBone <jakub.bone1990@gmail.com>
2025-09-29 09:38:34 +0200

    refactor: delete redundant code
    Former-commit-id: eff342787be488385a8361d452c21166240ea698

--- src/main/java/com/jakubbone/repository/MessageRepository.java
+++ src/main/java/com/jakubbone/repository/MessageRepository.java
@@ -1,11 +1,9 @@
 package com.jakubbone.repository;
 
 import com.jakubbone.model.Message;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
