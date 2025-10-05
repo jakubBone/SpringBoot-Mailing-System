1f71398a1106b635d11550b6aea74b5de7fe799f
jakubBone <jakub.bone1990@gmail.com>
2025-10-05 11:21:15 +0200

    feat: add RegisterRequest
    Former-commit-id: ebb9f7c19b75e424d2c6e46f035cea8ebcdc7367

---
+++ src/main/java/com/jakubbone/dto/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.jakubbone.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 10, message = "Username must be between 3 and 10 characters long")
+    @Pattern(regexp = "^[A-Za-z]{3,10}$",
+            message = "Username must contain only letters (A–Z), length 3–10")
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "First name cannot be blank")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be blank")
+    private String lastName;
+}
