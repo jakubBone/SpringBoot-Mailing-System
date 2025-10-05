3d69b1d4daefcf5e212d09303796add99758ef67
jakubBone <jakub.bone1990@gmail.com>
2025-10-05 11:15:33 +0200

    feat: add AuthService's registerUser method without logic yet

--- src/main/java/com/jakubbone/controller/AuthController.java
+++ src/main/java/com/jakubbone/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.jakubbone.controller;
 
+import com.jakubbone.service.AuthService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
 }
