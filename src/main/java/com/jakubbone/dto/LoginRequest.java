560b0a1c1a5b72182d9b617eebada7a15226fb2d
jakubBone <jakub.bone1990@gmail.com>
2025-10-10 15:41:09 +0200

    fix: add lacking semicolon
    feat: add constructor lombok annotation to reguests dto
    
    Former-commit-id: 4023ad9682e561d5c1f1c6fe90452bc596ddf5c9

--- src/main/java/com/jakubbone/dto/LoginRequest.java
+++ src/main/java/com/jakubbone/dto/LoginRequest.java
@@ -1,9 +1,15 @@
 package com.jakubbone.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "Username cannot be blank")
     private String username;
