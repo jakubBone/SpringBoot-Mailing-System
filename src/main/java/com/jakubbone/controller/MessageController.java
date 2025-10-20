e4f9148f8b3c234b2c0d13dd2a14581cf374adcc
jakubBone <jakub.bone1990@gmail.com>
2025-10-20 11:21:46 +0200

    docs: add OpenAPI documentation to controllers

--- src/main/java/com/jakubbone/controller/MessageController.java
+++ src/main/java/com/jakubbone/controller/MessageController.java
@@ -1,9 +1,14 @@
 package com.jakubbone.controller;
 
+import com.jakubbone.dto.ErrorResponse;
 import com.jakubbone.dto.MessageResponse;
 import com.jakubbone.dto.SendMessageRequest;
 import com.jakubbone.model.Message;
 import com.jakubbone.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,6 +35,36 @@ public class MessageController {
      * @return the created message with HTTP 201 status
      * @throws ResponseStatusException if recipient not found (404) or mailbox full (409)
      */
+    @Operation(
+            summary = "Send message",
+            description = "Sends messages to another user",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Message sent successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = MessageResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Recipient not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Users mailbox is full",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     @PostMapping
     public ResponseEntity<MessageResponse> sendMessage(@Valid @RequestBody SendMessageRequest req, Authentication authentication){
         JwtAuthenticationToken jwt = (JwtAuthenticationToken) authentication;
@@ -47,6 +82,29 @@ public class MessageController {
      * @param pageable pagination parameters (page, size, sort)
      * @return page of messages with HTTP 200 status
      */
+
+    @@Operation(
+            summary = "Read messages",
+            description = "Get message from logged user",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Return page of messages",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Page.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized user",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
    @GetMapping
    public ResponseEntity<Page<MessageResponse>> readMessages(Authentication authentication, Pageable pageable) {
         JwtAuthenticationToken jwt = (JwtAuthenticationToken) authentication;
@@ -64,6 +122,33 @@ public class MessageController {
      * @return HTTP 204 No Content on success
      * @throws ResponseStatusException if message not found (404) or access denied (403)
      */
+
+    @Operation(
+            summary = "Mark message as read",
+            description = "Marks a specific message as read",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Message read"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Access denied",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Message not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     @PatchMapping("/{id}/read")
     public ResponseEntity<Void> markMessageAsRead(@PathVariable Long id, Authentication authentication) {
         JwtAuthenticationToken jwt = (JwtAuthenticationToken) authentication;
@@ -82,6 +167,29 @@ public class MessageController {
      * @return page of matching messages with HTTP 200 status
      * @throws ResponseStatusException if phrase too short (400)
      */
+
+    @Operation(
+            summary = "Search messages",
+            description = "Searches messages by phrase",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Message found successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Page.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Phrase too short",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
     @GetMapping("/search")
     public ResponseEntity<Page<MessageResponse>> searchMessages(@RequestParam String phrase, Authentication authentication, Pageable pageable){
         JwtAuthenticationToken jwt = (JwtAuthenticationToken) authentication;
