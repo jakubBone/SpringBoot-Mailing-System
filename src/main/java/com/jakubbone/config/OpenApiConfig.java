f210addbfb0d87c8cc13e74d0d60622c40204ead
jakubBone <jakub.bone1990@gmail.com>
2025-10-20 10:14:03 +0200

    config: add OpenAPI configuration
    Former-commit-id: ad10c972e6d32e090344e80aa63fcd288fea54ae

--- src/main/java/com/jakubbone/config/OpenApiConfig.java
+++ src/main/java/com/jakubbone/config/OpenApiConfig.java
@@ -1,5 +1,25 @@
 package com.jakubbone.config;
 
-public class OpenApiConfig {
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot Mailing System API")
+                        .version("1.0.0")
+                        .description("API documentation for mailing system"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
 }
