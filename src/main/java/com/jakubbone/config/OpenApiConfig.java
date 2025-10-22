57db2a4db2f189e0b6a4deb3abbd7d3a5db96dd2
jakubBone <jakub.bone1990@gmail.com>
2025-10-22 11:18:08 +0200

    config: update OpenAPi with security item for keycloak auth
    fix: add mark Pageable parameter as @ParameterObject in conttroller methods

--- src/main/java/com/jakubbone/config/OpenApiConfig.java
+++ src/main/java/com/jakubbone/config/OpenApiConfig.java
@@ -1,17 +1,43 @@
 package com.jakubbone.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
+        String tokenUrl = "http://localhost:8180/realms/mailingsystem/protocol/openid-connect/token";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Spring Boot Mailing System API")
                         .version("1.0.0")
-                        .description("API documentation for mailing system"));
+                        .description("API documentation for mailing system"))
+
+
+                .addSecurityItem(new SecurityRequirement().addList("keycloak_oauth"))
+
+                .components(new Components()
+                        .addSecuritySchemes("keycloak_oauth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.OAUTH2)
+                                        .description("Keycloak OAuth2 - Resource Owner Password Flow")
+                                        .flows(new OAuthFlows()
+                                                .password(new OAuthFlow()
+                                                        .tokenUrl(tokenUrl)
+                                                        .scopes(new Scopes()
+                                                                .addString("openid", "OpenID Connect scope")
+                                                                .addString("profile", "Profile scope")
+                                                                .addString("email", "Email scope"))))));
     }
 }
